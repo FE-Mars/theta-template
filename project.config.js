1cc8feb3c6e3c6c74c3846f4dc4104846f4bd473
jwong <18810157617@163.com>
2019-03-01 10:13:21 +0800

    采用babel7

--- project.config.js
+++ project.config.js
@@ -1,22 +1,31 @@
 /**
+ * $XXX$标识需要自行配置的属性
  * 
- * proxyTarget 用于webpack-dev-server作代理
- * projectGroup  项目所属组   会根据此构造对应publicPath  以及  tpl_config中的key
+ * $GROUP$  项目所属组   会根据此构造对应publicPath  以及  tpl_config中的key
+ * $PROXYTARGET$ 用于webpack-dev-server作代理
  * 
  */
 module.exports = {
-  projectName: '{{name}}',        //项目名称
-  projectGroup: '',         //项目所属组    base crm  app paas等
+  projectName: 'object',        //项目名称
+  projectGroup: 'paas',         //项目所属组    base crm  app paas等
   moduleSeparator: '-',         //用于指定生成tpl_config的key时所用的模块分隔符，   目前paas采用”-“ 其他项目好像是”_“
   devDirectory: 'dev',          //开发目录
   prodDirectory: 'build',       //生产目录
-  runtimePublicPath: '',        //用于设置运行时publicPath  为空时通过 projectGroup  和 projectName 自动匹配
-  proxyTarget: '',                 //代理目标   （本地开发alpha的域名）
+  proxyTarget: 'https://www.dev.com',                 //代理目标   （本地开发alpha的域名）
   webpackConfig: {     //webpack的相关配置
-    entry: {},
+    entry: {
+      sdk: './src/sdk.js'
+    },
   
     externals: {  //不需要打包模块（外部依赖）
-      vue: 'base-vue'
+      vue: 'base-vue',
+      vuex: 'base-vuex',
+      'element-ui': 'base-element',
+      'crm-widget/table/table': 'crm-widget/table/table',
+    },
+
+    devServer: {
+      port: 8081
     }
   }
 }
